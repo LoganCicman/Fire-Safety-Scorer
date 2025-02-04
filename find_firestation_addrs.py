2ed7175a67f4d3faf754fb8985f22fa4c5adf11a
Logan Cicman <ljc2187@columbia.edu>
2025-02-04 10:00:26 -0500

    Added Fire Station GIS data to the repo and implemented retrieval of fire station data into a Pandas dataframe in find_firestation_addrs.

--- find_firestation_addrs.py
+++ find_firestation_addrs.py
@@ -1,1 +1,7 @@
-import pandas
+import pandas as pd
+
+if __name__ == "__main__":
+    #Import firestation data into a dataframe.
+    stations = pd.read_csv('data/NYS_Firestations.csv')
+
+    print(stations.loc[0]['Address'])
